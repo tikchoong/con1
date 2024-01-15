67dce3e66f94ef7c75b02227e28d44b86ea273f8
tc <tikchoong@gmail.com>
2024-01-15 18:35:23 +0800

    init

--- con1/Program.cs
+++ con1/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool exit = false;
+int i = 1;
+while (!exit)
+{
 
+    if (Console.KeyAvailable)
+    {
+        var c = Console.ReadKey().KeyChar;
+        if ((c == 'q') || (c == 'Q'))
+        {
+            exit = true;
+        }
+
+
+
+    }
+
+    Console.WriteLine($"Line {i++}");
+    Thread.Sleep(1000);
+
+}
+
+Console.WriteLine("Program exiting......");
+Console.ReadLine();
